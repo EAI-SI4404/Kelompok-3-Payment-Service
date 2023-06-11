aae5bffc5998f5a094546b07d7805bdd5ec3eb14
rahmandea <54786699+rahmandea@users.noreply.github.com>
2023-06-11 22:59:43 +0700

    Update bank_app.py

--- bank_app.py
+++ bank_app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'bank_iae'
+app.config['MYSQL_DB'] = 'bank_eai'
 mysql = MySQL(app)
 
 # Create Tables
@@ -27,25 +27,28 @@ def create_bank_payment():
     status = request.json['status']
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    message = ""
     
     cur = mysql.connection.cursor()
-    query = "SELECT * FROM account WHERE va = %s ;"
+    query = "SELECT * FROM account WHERE va = %s AND status = 'W';"
     cur.execute(query, [va])
     
     va_checked = cur.fetchall()
     if va_checked:
         response = requests.put('http://localhost:5000/updatebankpayment', json={
-        'addAmount': amount,
-        'update_time': update_time,
-        'va': va
-    })
-        return jsonify({'status_code': '201 Created', 'message': 'Transaction added successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 201
+            'addAmount': amount,
+            'update_time': update_time,
+            'va': va
+        })
+
+        message = "Transaction added successfully!"
     else:
         cur.execute("INSERT INTO account ( va, amount, title,status, time, update_time) VALUES ( %s, %s, %s, %s, %s, %s)", (va, amount, title,status, time, update_time))
         mysql.connection.commit()
         cur.close()
-        return jsonify({'status_code': '201 Created', 'message': 'Payment created successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 201
-
+        message = "Payment created successfully!"
+    
+    return jsonify({'status': True, 'status_code': 201, 'message': message, 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 201
 
 # Endpoint 2 - Update Bank Payment
 @app.route('/updatebankpayment', methods=['PUT'])
@@ -54,47 +57,54 @@ def update_bank_status():
     va = json_data['va']
     update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cur = mysql.connection.cursor()
+
+    str_sql = ""
+    message = ""
+    upd_val = ""
+
     if 'addAmount' in json_data:
-        addAmount = json_data['addAmount']
-        cur.execute("UPDATE account SET amount= amount + %s, update_time=%s WHERE va=%s", (addAmount, update_time, va))
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'status_code': '200 OK', 'message': 'Amount addition successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
+        upd_val = json_data['addAmount']
+        str_sql = "UPDATE account SET amount= amount + %s, update_time=%s WHERE va=%s AND status = 'W'"
+        message = "Amount addition successfully!"
     elif 'subAmount' in json_data:
-        subAmount = json_data['subAmount']
-        cur.execute("UPDATE account SET amount= amount - %s, update_time=%s WHERE va=%s", (subAmount, update_time, va))
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'status_code': '200 OK', 'message': 'Amount substraction successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
+        upd_val = json_data['subAmount']
+        str_sql = "UPDATE account SET amount= amount - %s, update_time=%s WHERE va=%s AND status = 'W'"
+        message = "Amount substraction successfully!"
     elif 'status' in json_data:
-        status = json_data['status']
-        cur.execute("UPDATE account SET status=%s, update_time=%s WHERE va=%s", (status, update_time, va))
-        mysql.connection.commit()
-        cur.close()
+        upd_val = json_data['status']
+        str_sql = "UPDATE account SET status=%s, update_time=%s WHERE va=%s AND status = 'W'"
+        message = "Status updated successfully!"
         response = requests.put('http://localhost:5000/updatepaymentstatus', json={
-        'va': va,
-        'status': status
-    })
-        return jsonify({'status_code': '200 OK', 'message': 'Status updated successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
+            'va': va,
+            'status': upd_val
+        })
+    
+    cur.execute(str_sql, (upd_val, update_time, va))
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({'status': True, 'status_code': 200, 'message': message, 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
 
 # Endpoint 3 - Delete Bank Payment
 @app.route('/deletebankpayment', methods=['DELETE'])
 def delete_bank_trans():
     va = request.json['va']
     cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM account WHERE va=%s", (va))
+    cur.execute("DELETE FROM account WHERE va=%s AND status = 'W'", (va))
     mysql.connection.commit()
     cur.close()
-    return jsonify({'status_code': '200 OK', 'message': 'Bank Payment deleted successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
+    return jsonify({'status': True, 'status_code': 200, 'message': 'Bank Payment deleted successfully!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 200
         
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({'status': False, 'status_code': 400,'message': 'Bad request!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 400
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'status_code': '404 Not Found','message': 'Endpoint not found!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 404
+    return jsonify({'status': False, 'status_code': 404,'message': 'Endpoint not found!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    return jsonify({'status_code': '500 Internal Server Error','message': 'Internal server error!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 500
+    return jsonify({'status': False, 'status_code': 500,'message': 'Internal server error!', 'timestamp' : datetime.now().strftime('%Y-%m-%d %H:%M:%S')}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
